b4463c0628a3d8987e21c91c00818afd3f5eec00
no <wunjacob@gmail.com>
2024-12-01 23:04:47 -0800

    Update offsets.hpp

--- dma-base/Source/offsets.hpp
+++ dma-base/Source/offsets.hpp
@@ -1,37 +1,42 @@
 #pragma once
-namespace offsets
+
+inline namespace offsets
 {
-    inline uintptr_t UWORLD = 0x1221b738; // I personally use 80085135
-    inline uintptr_t FNAME = 0x124E04C0;
-    inline uintptr_t GAME_INSTANCE = 0x1D8;
+    inline uintptr_t UWORLD = 0x13723C18; // I personally use 80085135
+    inline uintptr_t FNAME = 0x130C2A80;
+    inline uintptr_t GAME_INSTANCE = 0x200;
     inline uintptr_t LOCAL_PLAYERS = 0x38;
     inline uintptr_t PLAYER_CONTROLLER = 0x30;
-    inline uintptr_t LOCAL_PAWN = 0x338;
-    inline uintptr_t PAWN_PRIVATE = 0x308;
-    inline uintptr_t ROOT_COMPONENT = 0x198;
-    inline uintptr_t RELATIVE_LOCATION = 0x120;
-    inline uintptr_t PLAYER_STATE = 0x2B0;
-    inline uintptr_t TEAM_INDEX = 0x1201;
-    inline uintptr_t GAME_STATE = 0x160;
-    inline uintptr_t PLAYER_ARRAY = 0x2A8;
-    inline uintptr_t VELOCITY = 0x168;
-    inline uintptr_t MESH = 0x310;
+    inline uintptr_t LOCAL_PAWN = 0x2E8;
+    inline uintptr_t PAWN_PRIVATE = 0x320;
+    inline uintptr_t ROOT_COMPONENT = 0x1b0;
+    inline uintptr_t PLAYER_STATE = 0x2C8;
+    inline uintptr_t TEAM_INDEX = 0x1261;
+    inline uintptr_t GAME_STATE = 0x188;
+    inline uintptr_t PLAYER_ARRAY = 0x2C0;
+    inline uintptr_t VELOCITY = 0x180;
+    inline uintptr_t MESH = 0x328;
     inline uintptr_t ACTOR = 0xA0;
-    inline uintptr_t COMPONENT_TO_WORLD = 0x1C0;
-    inline uintptr_t BONE_ARRAY = 0x570;
-    inline uintptr_t BONE_ARRAY_CACHE = 0x600;
+    inline uintptr_t COMPONENT_TO_WORLD = 0x1E0;
+    inline uintptr_t BONE_ARRAY = 0x5A8;
+    inline uintptr_t BONE_ARRAY_CACHE = 0x5B8;
     inline uintptr_t LAST_SUMBIT_TIME = 0x2E0;
-    inline uintptr_t LAST_SUMBIT_TIME_ON_SCREEN = 0X2E8;
+    inline uintptr_t LAST_RENDER_TIME = 0x2F0;
     inline uintptr_t LocationUnderReticle = 0x2698;
-    inline uintptr_t IS_DYING = 0x6d0;
-    inline uintptr_t IS_BOT = 0x29A;
-    inline uintptr_t CURRENT_WEAPON = 0x9d8;
-    inline uintptr_t SPEED = 0x1CC4;
-    inline uintptr_t GRAV = 0x1F34;
-    inline uintptr_t TARGETED_PAWN = 0x1898;
-    inline uintptr_t LEVEL_ARRAY = 0x178;
-    inline uintptr_t LEVEL_COUNT = 0x180;
+    inline uintptr_t IS_DYING = 0x718;
+    inline uintptr_t IS_BOT = 0x2b2;
+    inline uintptr_t CURRENT_WEAPON = 0xa80;
+    inline uintptr_t SPEED = 0x1D70;
+    inline uintptr_t GRAV = 0x1DF0;
+    inline uintptr_t TARGETED_PAWN = 0x1920;
+    inline uintptr_t BUILDING_STATE = 0x1fe8;
+    inline uintptr_t LOCATION = 0x130;
+    inline uintptr_t ROTATION = 0x140;
+
+
+    inline uintptr_t LEVEL_ARRAY = 0x190;
+    inline uintptr_t LEVEL_COUNT = 0x192;
     inline uintptr_t FNAME_KEY = 0x18;
-    inline uintptr_t AACTOR_ARRAY = 0xA0;
-    inline uintptr_t AACTOR_COUNT = 0xA8;
+    inline uintptr_t AACTOR_ARRAY = 0xa0;
+    inline uintptr_t AACTOR_COUNT = 0xa8;
 }
